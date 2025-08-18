d9f1edec309a32c4057c3c464e973b9568f1aee4
ImrenR <imrenrahbay@gmail.com>
2025-08-18 23:47:14 +0100

    update

--- meal.js
+++ meal.js
@@ -50,7 +50,7 @@ document.querySelector("input").oninput=(e)=>{   // oninput is helps whatever yo
 
 // console.log(e) check what is called to call input = > target
 // console.log(arr) // now when you search on input you can see them on console
-dizi.meals.filter((a)=>a.strMeal.includes(a.target.value))
+let veri = dizi.meals.filter((a)=>a.strMeal.includes(a.target.value))
 showScreen(veri)
 } 
 
