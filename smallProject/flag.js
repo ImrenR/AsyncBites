baeff428ea28fda9a3b8d2c92fbed06ddcd237bf
ImrenR <imrenrahbay@gmail.com>
2025-08-22 13:10:02 +0100

    update

--- smallProject/flag.js
+++ smallProject/flag.js
@@ -1,18 +1,17 @@
 const getirCountry = async () => {
- const res = await fetch("https://restcountries.com/v3.1/all");
+  const res = await fetch("https://restcountries.com/v3.1/all");
 
- const veri = await res.json () // dont forget to change to json and add await
- bastirCountry(veri) // veri gonderildi asagida tanimla
+  const veri = await res.json(); // dont forget to change to json and add await
+  bastirCountry(veri); // veri gonderildi asagida tanimla
 };
-getirCountry()
+getirCountry();
 
- 
 //! to print on the screen
 
-const bastirCountry =(veri)=> { //* veri gonderildi sende karsila (veri) ama bu illa veri olmak zorunda degil
-veri.forEach((a) => {
-
-  document.querySelector('article').innerHTML = `
+const bastirCountry = (veri) => {
+  //* veri gonderildi sende karsila (veri) ama bu illa veri olmak zorunda degil
+  veri.forEach((a) => {
+    document.querySelector("article").innerHTML = `
 
   <div class="card text-start w-50 shadow-lg bg-light my-4" style="width: 18rem;">
   <img src="${a.flags.png}" class="card-img-top" alt="...">
@@ -24,13 +23,13 @@ veri.forEach((a) => {
  ${a.capital}
    </li>
   <li class="list-group-item">
-
+${Object.values(a.languages)}
   </li>
   <li class="list-group-item">  </li>
   
 </ul>
  </div>
 </div> 
-  `
-});
-}
+  `;
+  });
+};
